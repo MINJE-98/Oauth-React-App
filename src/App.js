f0d9113f7448afd05c5366c84b1327cbdbc0e716
MINJE-98 <jmj012100@gmail.com>
2021-07-06 20:11:41 +0900

    kakao login 테스트

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function loginButtonForKakao() {
+    window.location.href =
+      "https://kauth.kakao.com/oauth/authorize?client_id=95c2b6fc38e21b3d6d255e32020e7b75&redirect_uri=http://localhost:3001/auth&response_type=code";
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={loginButtonForKakao}>
+          <img src={logo} className="App-logo" alt="logo" />
+        </button>
       </header>
     </div>
   );
