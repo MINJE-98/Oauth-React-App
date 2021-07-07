af0472e4b4d8ad3c8e8bf8bef3739617f80e6147
MINJE-98 <jmj012100@gmail.com>
2021-07-07 20:05:54 +0900

    add index

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import GetToken from "./auth/getToken";
+import Home from "./home/home";
+import Navigationbar from "./Navigationbar/navigationbar";
+
+
+
+export {
+    GetToken,
+    Home,
+    Navigationbar
+}
+
