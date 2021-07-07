e8c02b2a77b9bce0f4c418020d347c75b3c2c3ad
MINJE-98 <jmj012100@gmail.com>
2021-07-07 20:03:46 +0900

    add store 및 persist, Thuck 장착

---
+++ src/redux/store/index.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from 'redux-thunk';
+import reducer from "../reducers";
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const enhancedReducer = persistReducer(persistConfig, reducer);
+
+export default createStore(enhancedReducer,applyMiddleware(ReduxThunk));
